03d950b2011ce6eac7a14ea3242ba3c459b5f793
phillip-rek <philliprekhotho@gmail.com>
2021-01-22 17:25:29 +0200

    created index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+let logo = document.getElementsByClassName("k-smbl")[0];
+let more = document.getElementsByClassName("more")[0];
+
+
+logo.className = "k-smbl dangle";
+// more.className = "more dangle";
