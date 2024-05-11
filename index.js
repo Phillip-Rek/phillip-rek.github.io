87db673346b0539d18013e2f2411666978a256e8
Phillip-Rek <philliprekhotho@gmail.com>
2024-05-12 00:23:40 +0200

    feat: display user guide info

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-let logo = document.getElementsByClassName("k-smbl")[0];
-let more = document.getElementsByClassName("more")[0];
+const playlistUsageInfoElement = document.createElement("div");
 
+const playlistElement = document.getElementById("playlist-element");
+const { x, width } = playlistElement.getBoundingClientRect();
 
-logo.className = "k-smbl dangle";
-// more.className = "more dangle";
+playlistUsageInfoElement.id = "playlist-usage-info"
+playlistUsageInfoElement.style.left = x + 20 + "px"
+const playlistUsageInfo = document.createElement("p")
+playlistUsageInfo.style.textAlign = "center";
+playlistUsageInfo.style.color = "#eeef";
+playlistUsageInfo.innerHTML = "The playlist is empty! Drop a video to the playlist"
+
+playlistUsageInfoElement.appendChild(playlistUsageInfo)
+document.body.appendChild(playlistUsageInfoElement);
+
+setTimeout(() => { playlistUsageInfoElement.remove() }, 3500)
+
+// setInterval(() => {
+//     if (document.getElementsByTagName("video").item(0).paused) {
+//         document.body.appendChild(playlistUsageInfoElement);
+
+//         setTimeout(() => { playlistUsageInfoElement.remove() }, 3500)
+//     }
+// }, 10000)
